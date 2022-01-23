34ca5f8e9729a6c07715cee650a8d5b60a4fee6e
DigitalFoxy <90634529+DigitalFoxy@users.noreply.github.com>
2022-01-23 17:29:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+function setButton() {
+setTimeout(
+  function pp1() {
+   document.getElementById("p1").innerHTML = "С";
+  }, 1000);
+setTimeout(
+  function pp2() {
+   document.getElementById("p2").innerHTML = "Днём";
+  }, 2500);
+  setTimeout(
+  function pp3() {
+   document.getElementById("p3").innerHTML = "Рождения";
+  }, 3500);
+  setTimeout(
+   function pp4() {
+    document.getElementById("p4").innerHTML = "Дима";
+   }, 4500);
+   setTimeout(
+   function pp5() {
+    document.getElementById("p5").innerHTML = "!";
+   }, 5500);
+   setTimeout(
+   function pp6() {
+    document.getElementById("p6").innerHTML = "...";
+   }, 6500);
+   setTimeout(
+   function pp7() {
+    document.getElementById("p7").innerHTML = "Поменьше кринжуй";
+   }, 7500);
+   setTimeout(
+   function pp8() {
+    document.getElementById("p8").innerHTML = "Побольше кринжуй";
+   }, 8500);
+   setTimeout(
+    function pp9() {
+     document.getElementById("p9").innerHTML = "Долги не копи";
+    }, 9500);
+    setTimeout(
+     function pp10() {
+      document.getElementById("p10").innerHTML = "и помни";
+     }, 10500);
+     setTimeout(
+     function pp11() {
+      document.getElementById("p11").innerHTML = "САЛФЕТКИ — СВЯТОЕ";
+     }, 11500);
+     setTimeout(
+      function pp12() {
+       document.getElementById("p13").innerHTML = "И есшо";
+      }, 12500);
+      setTimeout(
+       function pp14() {
+        document.getElementById("p14").innerHTML = "...";
+       }, 13500);
+       setTimeout(
+        function pp15() {
+         document.getElementById("p15").innerHTML = "РОМУ ОДНОГО В ГОРОДЕ БОЛЬШЕ НЕ ОСТАВЛЯЙ!";
+        }, 14500);
+}
